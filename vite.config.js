438f6b8ba7495a48163f89df11d11568d1e88ed3
rameshneel <ramesh@neelnewtworks.com>
2025-03-19 19:11:23 +0530

    fix: revert base path change in vite.config.js to "/booking/"

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
-//   base: "/booking/",
+//   base: "/booking/",// Change this from "/booking/" to "./"
 //   // server: {
 //   //   proxy: {
 //   //     "/": {
@@ -17,5 +17,5 @@ import react from "@vitejs/plugin-react";
 // });
 export default defineConfig({
   plugins: [react()],
-  base: "/booking/", // Change this from "/booking/" to "./"
+  base: "/booking/",
 });
